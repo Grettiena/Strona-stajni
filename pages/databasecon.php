30130ee5f6e62960c2076d9de87c813c9cbe50b6
Grettiena <102189425+Grettiena@users.noreply.github.com>
2022-06-30 16:20:28 +0200

    .

--- pages/databasecon.php
+++ pages/databasecon.php
@@ -1,8 +1,8 @@
 <?php
-    $servername='localhost';
-    $username='root';
-    $password='';
-    $dbname = "my_db";
+    $servername="localhost";
+    $username="root";
+    $password="";
+    $dbname = "stajnia";
     $conn=mysqli_connect($servername,$username,$password,"$dbname");
       if(!$conn){
           die('Could not Connect MySql Server:' .mysql_error());
